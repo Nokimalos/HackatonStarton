a5ceddfd6d1a8a3fb5564ed13c9cc9d09ad1e6a3
Illyas Chihi <illyas.chihi@epitech.eu>
2022-05-08 01:05:20 +0200

    [+] add db

--- vote_gouv/lib/main.dart
+++ vote_gouv/lib/main.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:vote_gouv/animation/launch_animation.dart';
+import 'package:vote_gouv/backend/citizen_info.dart';
+import 'package:vote_gouv/constants/citizen.dart';
 import 'package:vote_gouv/pages/home_page.dart';
 import 'constants/colors.dart';
 
-void main() {
+late Box box;
+
+Future<void> main() async {
+  await Hive.initFlutter();
+  box = await Hive.openBox('CitizenBox');
+  Hive.registerAdapter(CitizenInfoAdapter());
+  box.add(citizenIllyas);
+  box.add(citizenThomas);
+  box.add(citizenValentin);
+  box.add(citizenKaan);
+  box.add(citizenAntoine);
+  box.add(citizenMohammed);
   runApp(const MyApp());
 }
 
@@ -14,7 +28,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       routes: {
-        '/launch':(context) => const LaunchAnimPage(),
+        '/launch': (context) => const LaunchAnimPage(),
         '/home': (context) => const MyHomePage(),
         //'/vote':(context) =>
         //'/result':(context) =>
