8e9b1183a97eb31f7a5e59ef681af5dec6bbcb3f
Antoine GAVIRA-BOTTARI <antoine.gavira-bottari@epitech.eu>
2022-05-08 15:13:56 +0200

    [ADD] connected_page

---
+++ vote_gouv/lib/pages/connected_page.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:vote_gouv/constants/colors.dart';
+
+import '../widget/card_info.dart';
+
+class ConnectedPage extends StatelessWidget {
+  const ConnectedPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            const SizedBox(height: 25),
+            Text(
+              "Choisissez votre candidats",
+              style: GoogleFonts.bebasNeue(
+                textStyle: const TextStyle(
+                  letterSpacing: 6,
+                  fontSize: 45,
+                  color: Colors.black,
+                  fontWeight: FontWeight.w900,
+                ),
+              ),
+            ),
+            const SizedBox(height: 25),
+            MyCard(
+                imagelink: const AssetImage("assets/images/Jean-Luc-Melenchon.jpg"),
+                name: 'Jean-Luc-Melenchon',
+                color: pClearRed,
+                onTap: () {}),
+            const SizedBox(height: 15),
+            MyCard(
+                imagelink: const AssetImage("assets/images/marine.jpg"),
+                name: 'Marine Le Pen',
+                color: pBlackBlue,
+                onTap: () {}),
+            const SizedBox(height: 15),
+            MyCard(
+                imagelink: const AssetImage("assets/images/Nathalie-Arthaud.jpg"),
+                name: 'Nathalie Arthaud',
+                color: pBlackRed,
+                onTap: () {}),
+            const SizedBox(height: 15),
+            MyCard(
+                imagelink: const AssetImage("assets/images/nicolas.jpg"),
+                name: 'Nicolas Dupont-Aignan',
+                color: pClearBlue,
+                onTap: () {}),
+            const SizedBox(height: 15),
+            MyCard(
+                imagelink: const AssetImage("assets/images/hidalgo.jpg"),
+                name: 'Anne Hidalgo',
+                color: pClearRed,
+                onTap: () {}),
+            const SizedBox(height: 15),
+            MyCard(
+                imagelink: const AssetImage("assets/images/jadot.jpg"),
+                name: 'Yannick Jadot',
+                color: pClearGreen,
+                onTap: () {}),
+            const SizedBox(height: 15),
+            MyCard(
+                imagelink: const AssetImage("assets/images/lasalle.jpg"),
+                name: 'Jean Lassalle',
+                color: pClearBlue,
+                onTap: () {}),
+            const SizedBox(height: 15),
+            MyCard(
+                imagelink: const AssetImage("assets/images/macron.jpg"),
+                name: 'Emmanuel Macron',
+                color: pClearBlue,
+                onTap: () {}),
+            const SizedBox(height: 15),
+            MyCard(
+                imagelink: const AssetImage("assets/images/pecresse.jpg"),
+                name: 'Valérie Pécresse',
+                color: pBlackBlue,
+                onTap: () {}),
+            const SizedBox(height: 15),
+            MyCard(
+                imagelink: const AssetImage("assets/images/poutou.jpg"),
+                name: 'Philippe Poutou',
+                color: pBlackRed,
+                onTap: () {}),
+            const SizedBox(height: 15),
+            MyCard(
+                imagelink: const AssetImage("assets/images/roussel.jpg"),
+                name: 'Fabien Roussel',
+                color: pClearRed,
+                onTap: () {}),
+            const SizedBox(height: 15),
+            MyCard(
+                imagelink: const AssetImage("assets/images/zemour.jpg"),
+                name: 'Éric Zemmour',
+                color: pBlackBlue,
+                onTap: () {}),
+          ],
+        ));
+  }
+}
