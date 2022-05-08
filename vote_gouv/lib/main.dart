07280119c0a9d9005c1c49b8371620e60bf71cf9
Illyas Chihi <illyas.chihi@epitech.eu>
2022-05-08 10:19:15 +0200

    [+] add db

--- vote_gouv/lib/main.dart
+++ vote_gouv/lib/main.dart
@@ -1,23 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 import 'package:vote_gouv/animation/launch_animation.dart';
-import 'package:vote_gouv/backend/citizen_info.dart';
-import 'package:vote_gouv/constants/citizen.dart';
 import 'package:vote_gouv/pages/home_page.dart';
 import 'constants/colors.dart';
 
-late Box box;
 
 Future<void> main() async {
-  // await Hive.initFlutter();
-  // box = await Hive.openBox('CitizenBox');
-  // Hive.registerAdapter(CitizenInfoAdapter());
-  // box.add(citizenIllyas);
-  // box.add(citizenThomas);
-  // box.add(citizenValentin);
-  // box.add(citizenKaan);
-  // box.add(citizenAntoine);
-  // box.add(citizenMohammed);
   runApp(const MyApp());
 }
 
