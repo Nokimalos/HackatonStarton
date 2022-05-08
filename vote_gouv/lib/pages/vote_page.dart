62d9726372d3d346968b48ab49e92ab95efd622d
Valou <valentin.fouillet@epitech.eu>
2022-05-08 11:00:53 +0200

    [ADD] caca modif page statement

--- vote_gouv/lib/pages/vote_page.dart
+++ vote_gouv/lib/pages/vote_page.dart
@@ -11,55 +11,34 @@ class MyVotePage extends StatefulWidget {
   State<MyVotePage> createState() => _MyVotePageState();
 }
 
-
-
 class _MyVotePageState extends State<MyVotePage> {
-  static final List<Widget> _widgetOptions = <Widget>[
-
-  ];
-
-  int _selectedIndex = 0;
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: MyBottomNavBar(
-        color: clearBlue,
-        onTap: _onItemTapped,
-        selectedIndex: _selectedIndex,
-      ),
-      body: Container(
-         padding: const EdgeInsets.all(25.0),
-         child: SingleChildScrollView(
-             scrollDirection: Axis.vertical,
-             child: Column(
-               children: const [
-                SizedBox(height: 15),
-                MyCard(imagelink: AssetImage("assets/images/nicolas.jpg"),name: 'Nicolas Dupont-Aignan', color: pClearBlue),
-                SizedBox(height: 15),
-                MyCard(imagelink: AssetImage("assets/images/hidalgo.jpg"),name: 'Anne a Hidalgo', color: pClearRed),
-                SizedBox(height: 15),
-                MyCard(imagelink: AssetImage("assets/images/jadot.jpg"),name: 'Yannick Jadot', color: pClearGreen),
-                SizedBox(height: 15),
-                MyCard(imagelink: AssetImage("assets/images/lasalle.jpg"),name: 'Jean Lassalle', color: pClearBlue),
-                SizedBox(height: 15),
-                MyCard(imagelink: AssetImage("assets/images/macron.jpg"),name: 'Emmanuel Macron', color: pClearBlue),
-                SizedBox(height: 15),
-                MyCard(imagelink: AssetImage("assets/images/pecresse.jpg"),name: 'Valérie Pécresse', color: pBlackBlue),
-                SizedBox(height: 15),
-                MyCard(imagelink: AssetImage("assets/images/poutou.jpg"),name: 'Philippe Poutou', color: pBlackRed),
-                SizedBox(height: 15),
-                MyCard(imagelink: AssetImage("assets/images/roussel.jpg"),name: 'Fabien Roussel', color: pClearRed),
-                SizedBox(height: 15),
-                MyCard(imagelink: AssetImage("assets/images/zemour.jpg"),name: 'Éric Zemmour', color: pBlackBlue),
-              ],
-            )
-          ),
+      body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: const [
+             SizedBox(height: 15),
+             MyCard(imagelink: AssetImage("assets/images/nicolas.jpg"),name: 'Nicolas Dupont-Aignan', color: pClearBlue),
+             SizedBox(height: 15),
+             MyCard(imagelink: AssetImage("assets/images/hidalgo.jpg"),name: 'Anne a Hidalgo', color: pClearRed),
+             SizedBox(height: 15),
+             MyCard(imagelink: AssetImage("assets/images/jadot.jpg"),name: 'Yannick Jadot', color: pClearGreen),
+             SizedBox(height: 15),
+             MyCard(imagelink: AssetImage("assets/images/lasalle.jpg"),name: 'Jean Lassalle', color: pClearBlue),
+             SizedBox(height: 15),
+             MyCard(imagelink: AssetImage("assets/images/macron.jpg"),name: 'Emmanuel Macron', color: pClearBlue),
+             SizedBox(height: 15),
+             MyCard(imagelink: AssetImage("assets/images/pecresse.jpg"),name: 'Valérie Pécresse', color: pBlackBlue),
+             SizedBox(height: 15),
+             MyCard(imagelink: AssetImage("assets/images/poutou.jpg"),name: 'Philippe Poutou', color: pBlackRed),
+             SizedBox(height: 15),
+             MyCard(imagelink: AssetImage("assets/images/roussel.jpg"),name: 'Fabien Roussel', color: pClearRed),
+             SizedBox(height: 15),
+             MyCard(imagelink: AssetImage("assets/images/zemour.jpg"),name: 'Éric Zemmour', color: pBlackBlue),
+           ],
+         )
        ),
     );
   }
